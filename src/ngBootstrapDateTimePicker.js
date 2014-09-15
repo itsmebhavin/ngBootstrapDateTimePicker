bcf95f1dd3208e1c137c73c3e8b925798a5e508e
Bhavin Patel <bpatel@cs.ua.edu>
2014-09-15 12:21:06 -0500

    fixing use strict.

--- src/ngBootstrapDateTimePicker.js
+++ src/ngBootstrapDateTimePicker.js
@@ -1,4 +1,20 @@
-var appRoot = angular.module("ngBootstrapDateTimePicker",[])
+'use strict';
+
+/**
+ * @description
+ * AUTHOR: Bhavin Patel (CAPS, University of Alabama)
+ * Download & more info at 
+ *      https://github.com/bapatel1/angular-bootstrap-datetimepicker
+ * This module is depending on jQuery's bootstrap-datetimepicker.js 
+ * for information regarding bootstrap-datetimepicker, please visit - 
+ *       http://eonasdan.github.io/bootstrap-datetimepicker
+ * I have just created directives so that we can use this widget in our
+ * angular web apps.
+ *
+ * system for displaying notifications of varying degree to it's users. 
+ *
+ */
+angular.module('ngBootstrapDateTimePicker', [])
 .directive('datez', function () {
     return {
         restrict: 'A',
@@ -21,8 +37,9 @@ var appRoot = angular.module("ngBootstrapDateTimePicker",[])
         require: 'ngModel',
         link: function (scope, element, attrs, ngModelCtrl) {
             element.datetimepicker({
-                dateFormat: 'dd/MM/yyyy hh:mm:ss',
-                language: 'en'
+                dateFormat: 'MM/dd/yyyy hh:mm:ss',
+                language: 'en',
+                pickTime: true
             }).on('changeDate', function (e) {
                 ngModelCtrl.$setViewValue(e.date);
                 scope.$apply();
